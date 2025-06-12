db8ce18317bb18d39d5a7a8e0c616a310836d9ee
Sofia <sofiacasta00@gmail.com>
2025-06-11 23:03:34 -0300

    feat: usuarios

--- src/model/usuario/UserCliente.java
+++ src/model/usuario/UserCliente.java
@@ -1,4 +1,32 @@
 package model.usuario;
 
-public class UserCliente {
+import enums.TipoUsuario;
+import model.Carrito;
+import model.Orden;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserCliente extends Usuario {
+    private Carrito carrito;
+    private List<Orden> historial;
+
+    public UserCliente(String gmail, String contraseña) {
+        super(gmail, contraseña, TipoUsuario.CLIENTE);
+        this.carrito = new Carrito();
+        this.historial = new ArrayList<>();
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public List<Orden> getHistorial() {
+        return historial;
+    }
+
+//    @Override
+//    public void getMenu() {
+//        MenuCliente menu = new MenuCliente();
+//        menu.mostrarMenu();
+//    }
 }
